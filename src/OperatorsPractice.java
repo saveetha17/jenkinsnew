0996c6d40c2c5614a2b74b907bc253fee5dbb9a6
saveetha90 <saveethakathiravan@gmail.com>
2021-05-20 20:59:05 -0400

    Fourth Commit

--- src/OperatorsPractice.java
+++ src/OperatorsPractice.java
@@ -33,6 +33,6 @@ public class OperatorsPractice {
 //        System.out.println(a!=b);
 //        System.out.println(a>=b);
 //        System.out.println(a<=b);
-//        System.out.println("Logical AND " +(a==b && a!=b && a>=b && a<=b));
+          System.out.println("Logical AND " +(a==b && a!=b && a>=b && a<=b));
     }
 }
